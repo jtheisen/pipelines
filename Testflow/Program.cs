9c474ca9dbab7b7e7a821b89ff058fee2cb2eabc
Jens Theisen <pass-github@zokfotpik.de>
2023-08-05 17:42:25 +0200

    Add testflow copy test app

---
+++ Testflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System.Text;
+using Testflow;
+
+Console.WriteLine("Hello, World!");
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddCommandLine(args)
+    .Build();
+
+var settings = new AppSettings();
+
+configuration.Bind(settings);
+
+Console.WriteLine($"Let's go with: {settings.StorageConnectionString} and {settings.StorageConnectionString}");
+
+var blobClient = new BlobClient(settings.StorageConnectionString, "testflow_main_db", "testcase");
+
+blobClient.Upload(new MemoryStream(Encoding.UTF8.GetBytes("Hello, World!")));
+
+Console.WriteLine("Blob written");
+
+var connection = new MySqlConnection(settings.TestflowDbConnectionString);
+
+var result = connection.QuerySingle("select count(*) from testcase;");
+
+Console.WriteLine("Result from Mysql: " + result);
