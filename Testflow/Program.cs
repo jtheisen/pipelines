f03d21dc456cc7f7b40b7d8805d1d21f9eb815ad
Jens Theisen <pass-github@zokfotpik.de>
2023-08-07 01:03:01 +0200

    wip

--- Testflow/Program.cs
+++ Testflow/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Storage.Blobs;
 using Dapper;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using MySqlConnector;
 using System.Text;
@@ -19,9 +20,32 @@ configuration.Bind(settings);
 Console.WriteLine($"Let's go with: {settings.TargetDbConnectionString} and {settings.TestflowDbConnectionString}");
 
 
+var sourceConnection = new MySqlConnection(settings.TestflowDbConnectionString);
+sourceConnection.Open();
 
-var connection = new MySqlConnection(settings.TestflowDbConnectionString);
+var columns = new[] {
+    "id",
+    "company", "location", "event", "product", "testuser", "offer_id", "invoice_id", "laboratory_id",
+    "timeslot", "timeslot_end", "checkin_at", "test_date", "result_date", "consent_at"
+};
 
-var result = connection.QuerySingle("select count(*) from testcases_archive;");
+var columnsSql = String.Join(", ", columns);
 
-Console.WriteLine("Result from Mysql: " + result);
+var readingCommandSql = $"select {columnsSql} from testcases_archive where id > 0 limit 10;";
+
+//var batch = sourceConnection.Query(readingCommand);
+
+var readingCommand = new MySqlCommand(readingCommandSql, sourceConnection);
+
+var reader = readingCommand.ExecuteReader();
+
+using var targetConnection = new SqlConnection(settings.TargetDbConnectionString);
+targetConnection.Open();
+using var sqlBulkCopy = new SqlBulkCopy(targetConnection);
+sqlBulkCopy.DestinationTableName = "testcases_archive";
+sqlBulkCopy.WriteToServer(reader);
+
+//targetConnection.Execute($"inset into testcases_archive ({columnsSql}) values ()");
+
+
+Console.WriteLine("done");
